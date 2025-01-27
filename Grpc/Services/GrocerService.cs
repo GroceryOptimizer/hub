4be2ff30a5cac6dc61f2c8244ccb28466da80ed0
Viktor Näslund <1299835+Raiu@users.noreply.github.com>
2025-01-27 10:07:41 +0100

    add scaff for grpc

---
+++ Grpc/Services/GrocerService.cs
@@ -0,0 +1,19 @@
+namespace Grpc.Services;
+
+public class GrocerService : HubStoreService.HubStoreServiceBase
+{
+    public GrocerService()
+    {
+
+    }
+
+    public Task<HealthCheckResponse> CheckIn(Store store) {
+
+        return Task.FromResult(new HealthCheckResponse {
+            StoreId = store.Id,
+            
+        });
+
+    }
+    
+}
