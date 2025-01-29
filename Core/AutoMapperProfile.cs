727e5cb7d070e4a84a7e127b2d22da206353f9da
vikkoooo <vikkolundberg@gmail.com>
2025-01-29 08:35:04 +0100

    add automapper

---
+++ Core/AutoMapperProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+
+using Core.DTOs;
+using Core.Entities;
+
+namespace Core
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // Entity → DTO
+            CreateMap<Coordinates, CoordinatesDTO>().ReverseMap();
+
+            CreateMap<Product, ProductDTO>().ReverseMap();
+
+            CreateMap<StockItem, StockItemDTO>()
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Product.Id))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+                .ReverseMap();
+
+            CreateMap<ShoppingCart, ShoppingCartDTO>().ReverseMap();
+
+            CreateMap<Vendor, VendorDTO>().ReverseMap();
+
+            CreateMap<VendorVisit, VendorVisitDTO>()
+                .ForMember(dest => dest.VendorId, opt => opt.MapFrom(src => src.Vendor.Id))
+                .ForMember(dest => dest.VendorName, opt => opt.MapFrom(src => src.Vendor.Name))
+                .ReverseMap();
+        }
+    }
+}
