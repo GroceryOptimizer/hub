a12e8f7f807c0be0b62ba486157e5d4d9e70ba14
vikkoooo <vikkolundberg@gmail.com>
2025-01-29 09:00:48 +0100

    add base dbcontext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<StockItem> StockItems { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<VendorVisit> VendorVisits { get; set; }
+        public DbSet<Coordinates> Coordinates { get; set; }
+    }
+}
