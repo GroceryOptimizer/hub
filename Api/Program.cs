ef312c715df50d55b039e26303d808ac792ca408
vikkoooo <vikkolundberg@gmail.com>
2025-01-29 11:39:13 +0100

    temporary switch database type

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,8 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Services
 // Database
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? throw new InvalidOperationException("Connection string not found.")));
+
 
 // Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
