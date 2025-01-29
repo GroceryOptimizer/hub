657dc05b0035a9f9f81f3bacb4c00ff039ca0d21
vikkoooo <vikkolundberg@gmail.com>
2025-01-29 09:06:20 +0100

    add db context to services

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,26 @@
 using Core;
 
+using Data;
+
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Services
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// AutoMapper
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+// Controllers
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -19,7 +32,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
