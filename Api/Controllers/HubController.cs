36c5b9294af18ed514cff3545885c3643eabf7db
RikiRhen <riki.rhen@live.se>
2025-02-10 15:47:05 +0100

    Endpoint maybe works? Needs testing

--- Api/Controllers/HubController.cs
+++ Api/Controllers/HubController.cs
@@ -119,6 +119,10 @@ namespace Api.Controllers
         public async Task<ActionResult<IEnumerable<VendorVisitDTO>>> PostCart(ShoppingCartDTO cart)
         {
             // todo: early exit if bad input
+            if (cart == null || cart.Cart == null || cart.Cart.Count == 0)
+            {
+                return BadRequest("Cart was found empty or nonexistant.");
+            }
             // pass cart to GRPC API call
                 //Parse DTO cart to normal Products
             List<Product> products = cart.Cart.Select(p => new Product(p.Name)).ToList();
@@ -126,11 +130,31 @@ namespace Api.Controllers
             ShoppingCart cartToSend = new ShoppingCart(products);
             //Call the gRPC function to send the request and await response from GRPC API
             Dictionary<int, List<StockItemDTO>> result = await StoreController.SendGrpcCall(cartToSend);
-            // parse response to VendorVisitDTOs (find vendor in db by vendor id)
+            // get our relevant vendors ready and prepared
+            var vendorIds = result.Keys.ToList();
+            var relevantVendors = await _context.Vendors
+                                    .Include(v => v.Coordinates)
+                                    .Where(v => vendorIds.Contains(v.Id))
+                                    .ToListAsync();
+            // parse response from gRPC to VendorVisitDTOs (find vendor in db by vendor id)
+            List<VendorVisitDTO> returnList = relevantVendors.Select(v =>
+                new VendorVisitDTO(
+                    v.Id,
+                    new VendorDTO(
+                        v.Id,
+                        v.Name,
+                        v.CoordinatesId,
+                            new CoordinatesDTO(
+                                v.Coordinates.Id,
+                                v.Coordinates.Latitude,
+                                v.Coordinates.Longitude
+                            )
+                        ),
+                    result.ContainsKey(v.Id) ? result[v.Id] : new List<StockItemDTO>()
+                    )
+                ).ToList();
 
-            // create VendorVisitDTO list to return
-
-            return null;
+            return Ok(returnList);
         }
 
         //[HttpGet("AnotherTestGet")]
