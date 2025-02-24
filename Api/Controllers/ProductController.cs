c2726e5a134f71e97baa70f167dd36572b68f98d
vikkoooo <vikkolundberg@gmail.com>
2025-02-24 11:53:24 +0100

    create controller which returns product dtos

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Core.DTOs;
+
+using Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET all products
+        [HttpGet]
+        public async Task<ActionResult<List<ProductDTO>>> GetAllProducts()
+        {
+            // Get all stock items from stocklist
+            var stockList = await _context.StockList.Include(s => s.StockItems).ThenInclude(p => p.Product).ToListAsync();
+
+            // Make products from the stocklist
+            var productList = stockList.SelectMany(s => s.StockItems).Select(p => new ProductDTO(p.Product.Name)).ToList() ?? new List<ProductDTO>();
+
+            return Ok(productList);
+        }
+
+    }
+}
